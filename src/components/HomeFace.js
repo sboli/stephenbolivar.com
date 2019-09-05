98dbfe5ee3be121180c5f88f4cd115e3c988ef17
Stephen Bolivar <bolivar.stephen@gmail.com>
2019-09-05 06:41:18 -0400

    double border around avatar

--- src/components/HomeFace.js
+++ src/components/HomeFace.js
@@ -1,40 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Ring = styled.div``;
-
 const FaceImg = styled.img`
     display: block;
     height: 100%;
     width: auto;
     border-radius: 50%;
-    padding: 7.5px;
-`;
-
-const Root = styled.div`
-    background-color: red;
-    width: 100%;
-    height: 100%;
-`;
-
-const Outer = styled.div`
-    border: 30px solid rgba(0, 0, 0, 0.04);
-`;
-
-const Inner = styled.div`
-    width: 200px;
-    height: 200px;
-    background-color: #00000020;
-    border-radius: 50%;
-    margin: 7.5px;
+    margin-left: auto;
+    margin-right: auto;
+    box-shadow: 0 0 0 15px hsl(0, 0%, 85%), 0 0 0 30px hsl(0, 0%, 95%);
 `;
 
 export default function HomeFace() {
     return (
         <>
-            <Outer>
-                <FaceImg src="/images/face.jpg"></FaceImg>
-            </Outer>
+            <FaceImg src="/images/face.jpg"></FaceImg>
         </>
     );
 }

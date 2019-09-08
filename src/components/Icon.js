df8060b61da3c136ea28ba833697eb72bc53a87c
Stephen Bolivar <bolivar.stephen@gmail.com>
2019-09-08 00:38:25 -0400

    colorized icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import * as feather from 'feather-icons';
+import theme from '../theme';
 
 export default function Icon({ name, size }) {
     return (
         <>
             <div
                 dangerouslySetInnerHTML={{
-                    __html: feather.icons[name].toSvg({ width: size || 48, height: size || 48 })
+                    __html: feather.icons[name].toSvg({
+                        width: size || 48,
+                        height: size || 48,
+                        color: theme.textPrimary
+                    })
                 }}
             ></div>
         </>

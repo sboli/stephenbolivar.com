dd487fea1a46df28d2cb33fdfb64523ad06c09e7
Stephen BOLIVAR <stephen@sacerdos.fr>
2019-09-14 14:13:06 -0400

    remove one anim on menu close

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -86,7 +86,7 @@ export default function Menu() {
     const transitions = useTransition(menuItems, item => item.id, {
         from: { opacity: 0, transform: 'translate3d(0,-40px,0)' },
         enter: { opacity: 1, transform: 'translate3d(0,0px,0)' },
-        leave: { opacity: 0, transform: 'translate3d(0,-40px,0)' },
+        leave: { opacity: 0 },
         trail: 150,
         reset: state.isMenuOpen
     });

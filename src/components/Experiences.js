d1c860ac6588eeaa3d8ff1934b088f966eb2175d
Stephen BOLIVAR <stephen@sacerdos.fr>
2019-09-15 19:26:33 -0400

    responsive

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -15,7 +15,7 @@ const Root = styled(animated.div)`
     @media ${device.laptopL} {
         max-width: 70%;
     }
-    @media ${device.tablet} {
+    @media ${device.laptop} {
         grid-template-columns: 1fr 1fr;
         grid-template-rows: auto auto;
     }
@@ -23,9 +23,10 @@ const Root = styled(animated.div)`
     grid-gap: 2rem;
 `;
 
-const MissionsTitle = styled(animated.h2)`
+const MissionsTitle = styled(animated.h1)`
     grid-column: 1/2;
     grid-row: 1/2;
+    margin-top: 2rem;
 `;
 
 const Missions = styled(animated.div)`
@@ -37,11 +38,19 @@ const Missions = styled(animated.div)`
     grid-row: 2/3;
 `;
 
+const EducationTitle = styled.h1`
+    @media ${device.laptop} {
+        grid-column: 2/3;
+        grid-row: 1/2;
+    }
+    margin-top: 2rem;
+`;
+
 const Education = styled(animated.div)`
     display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: repeat(6, 1fr);
-    @media ${device.tablet} {
+    @media ${device.laptop} {
         grid-column-gap: 4rem;
         grid-row-gap: 1rem;
         grid-template-columns: 1fr 1fr;
@@ -58,8 +67,8 @@ export default function Experiences({ style }) {
                     <ExperiencesMission {...it}></ExperiencesMission>
                 ))}
             </Missions>
+            <EducationTitle>Éducation</EducationTitle>
             <Education></Education>
-            <h2 style={{ marginBottom: '2rem', marginTop: '5rem' }}>Éducation</h2>
         </Root>
     );
 }

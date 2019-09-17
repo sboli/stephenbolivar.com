1964900505931fdd52ff2349742b875cb74381de
Stephen BOLIVAR <stephen@sacerdos.fr>
2019-09-17 13:02:21 -0400

    extract skill data

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import device from '../mediaqueries';
 import Pie from './Pie';
 import ProgressBar from './ProgressBar';
+import { technical, professional } from '../skills';
 
 const Root = styled(animated.div)`
     grid-column: 1/2;
@@ -64,24 +65,24 @@ export default function Skills({ style }) {
         to: { transform: 'translate3d(0, 0, 0)', opacity: 1 },
         config: config.slow
     });
+    let odd = false;
     return (
         <Root style={style}>
             <h2 style={{ marginBottom: '2rem' }}>Compétences techniques</h2>
             <Technical>
-                <ProgressBar style={fromLeftStyle[0]} title="ReactJS" progress={0.9}></ProgressBar>
-                <ProgressBar style={fromRightStyle[0]} title="Javascript" progress={0.9}></ProgressBar>
-                <ProgressBar style={fromLeftStyle[1]} title="PHP" progress={0.8}></ProgressBar>
-                <ProgressBar style={fromRightStyle[1]} title="HTML/CSS" progress={0.75}></ProgressBar>
-                <ProgressBar style={fromLeftStyle[2]} title="GNU/Linux" progress={0.9}></ProgressBar>
-                <ProgressBar style={fromRightStyle[2]} title="Bureautique" progress={0.4}></ProgressBar>
+                {technical.map((it, index) => {
+                    odd = !odd;
+                    return (
+                        <ProgressBar style={odd ? fromLeftStyle[it.row] : fromRightStyle[it.row]} {...it}></ProgressBar>
+                    );
+                })}
             </Technical>
 
             <h2 style={{ marginBottom: '2rem', marginTop: '5rem' }}>Compétences professionnelles</h2>
             <Professional>
-                <Pie style={fromBottomStyle[0]} title="Travail en  équipe" progress={0.9}></Pie>
-                <Pie style={fromBottomStyle[1]} title="Gestion  de projet" progress={0.8}></Pie>
-                <Pie style={fromBottomStyle[2]} title="Résolution de problèmes" progress={0.9}></Pie>
-                <Pie style={fromBottomStyle[3]} title="Sens artistique" progress={0.6}></Pie>
+                {professional.map((it, index) => (
+                    <Pie style={fromBottomStyle[index]} {...it}></Pie>
+                ))}
             </Professional>
         </Root>
     );
---
+++ src/skills.js
@@ -0,0 +1,51 @@
+export const technical = [
+    {
+        title: 'ReactJS',
+        progress: 0.9,
+        row: 0
+    },
+    {
+        title: 'Javascript',
+        progress: 0.9,
+        row: 0
+    },
+    {
+        title: 'PHP',
+        progress: 0.8,
+        row: 1
+    },
+    {
+        title: 'HTML/CSS',
+        progress: 0.75,
+        row: 1
+    },
+    {
+        title: 'GNU/Linux',
+        progress: 0.9,
+        row: 2
+    },
+    {
+        title: 'Bureautique',
+        progress: 0.4,
+        row: 2
+    }
+];
+
+export const professional = [
+    {
+        title: 'Travail en équipe',
+        progress: 0.9
+    },
+    {
+        title: 'Gestion de projet',
+        progress: 0.8
+    },
+    {
+        title: 'Résolution de problèmes',
+        progress: 0.9
+    },
+    {
+        title: 'Sens artistique',
+        progress: 0.6
+    }
+];

e5107bcc8d5a232dd1c5ebf92a922530c4b2b68a
Stephen BOLIVAR <stephen@sacerdos.fr>
2019-09-20 23:11:59 -0400

    4th item

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,13 +3,13 @@ export default [
         title: 'Ce portfolio',
         url: 'https://stephenbolivar.com',
         image: '/images/portfolio.1.png',
-        description: `Une PWA réalisée avec ReactJS et déployée sur netlify. <br>Code open-souce, hébergé sur github.com`
+        description: `Une PWA réalisée avec ReactJS et déployée sur netlify.`
     },
     {
         title: 'Tiling Window Manager Notification',
         url: 'https://github.com/sboli/twmn',
         image: '/images/portfolio.2.png',
-        description: `Une application client-serveur permettant d'afficher des notifications
+        description: `Une application C++/Qt, client-serveur permettant d'afficher des notifications
         correctement intégrées sur les gestionnaires des fenêtres dont tout l'espace est occupé par le contenu.`
     },
     {
@@ -19,6 +19,9 @@ export default [
         description: `Un jeu de dominos sur Android réalisé en Java avec <a href="https://libgdx.badlogicgames.com/">libGDX</a>, et propulsé par un serveur NodeJS avec une communication WebSocket.`
     },
     {
-        title: ''
+        title: 'Frontend OCS Ericsson',
+        url: undefined,
+        image: '/images/portfolio.4.png',
+        description: `Une application PHP/Javascript qui permet de manager l'OCS Ericsson via son api XMLRPC`
     }
 ];

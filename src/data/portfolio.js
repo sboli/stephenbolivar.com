9c6fb4ec310d224a1c3a2291382ae6cb6463604a
Stephen BOLIVAR <stephen@sacerdos.fr>
2019-11-08 10:21:08 -0400

    add 2 portfolio items

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,27 +1,40 @@
 export default [
-    {
-        title: 'Ce portfolio',
-        url: 'https://stephenbolivar.com',
-        image: '/images/portfolio.1.png',
-        description: `Une PWA réalisée avec ReactJS et déployée sur netlify.`
-    },
-    {
-        title: 'Tiling Window Manager Notification',
-        url: 'https://github.com/sboli/twmn',
-        image: '/images/portfolio.2.png',
-        description: `Une application C++/Qt, client-serveur permettant d'afficher des notifications
+  {
+    title: "Ce portfolio",
+    url: "https://stephenbolivar.com",
+    image: "/images/portfolio.1.png",
+    description: `Une PWA réalisée avec ReactJS et déployée sur netlify.`
+  },
+  {
+    title: "Frontend Vue.js",
+    url: "https://admin.zeewifi.com",
+    image: "/images/portfolio.5.png",
+    description: `Un tableau de bord réalisé avec VueJs. Backend en laravel.`
+  },
+  {
+    title: "Frontend Nuxt.js",
+    url: undefined,
+    image: "/images/portfolio.6.png",
+    description: `Un portail captif réalisé avec NuxtJS + Vuex. Backend en laravel.`
+  },
+  {
+    title: "Tiling Window Manager Notification",
+    url: "https://github.com/sboli/twmn",
+    image: "/images/portfolio.2.png",
+    description: `Une application C++/Qt, client-serveur permettant d'afficher des notifications
         correctement intégrées sur les gestionnaires des fenêtres dont tout l'espace est occupé par le contenu.`
-    },
-    {
-        title: 'Dominoes multiplayer',
-        url: 'https://play.google.com/store/apps/details?id=com.enjoyinformatique.dwf',
-        image: '/images/portfolio.3.png',
-        description: `Un jeu de dominos sur Android réalisé en Java avec <a href="https://libgdx.badlogicgames.com/">libGDX</a>, et propulsé par un serveur NodeJS avec une communication WebSocket.`
-    },
-    {
-        title: 'Frontend OCS Ericsson',
-        url: undefined,
-        image: '/images/portfolio.4.png',
-        description: `Une application PHP/Javascript qui permet de manager l'OCS Ericsson via son api XMLRPC`
-    }
+  },
+  {
+    title: "Dominoes multiplayer",
+    url:
+      "https://play.google.com/store/apps/details?id=com.enjoyinformatique.dwf",
+    image: "/images/portfolio.3.png",
+    description: `Un jeu de dominos sur Android réalisé en Java avec <a href="https://libgdx.badlogicgames.com/">libGDX</a>, et propulsé par un serveur NodeJS avec une communication WebSocket.`
+  },
+  {
+    title: "Frontend OCS Ericsson",
+    url: undefined,
+    image: "/images/portfolio.4.png",
+    description: `Une application PHP/Javascript qui permet de manager l'OCS Ericsson via son api XMLRPC`
+  }
 ];

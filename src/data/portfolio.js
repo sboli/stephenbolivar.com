ac01afec3396a93a3373f6dbd4847f476a76cc2a
Stephen BOLIVAR <bolivar.stephen@gmail.com>
2021-03-01 20:31:25 -0400

    add portfolio items

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,53 +1,59 @@
 const Portfolio = [
   {
+    title: 'Tableau de bord marketing',
+    url: 'https://sofy.fr',
+    image: '/images/portfolio.10.png',
+    description: `Une plateforme marketing 360. Campagnes SMS, campagnes email, campagnes push. Click & Collect. Fidélité.<br><small>Vue, Nest.js, Redis, Mysql, Docker.</small>`,
+  },
+  {
+    title: 'Application Budy',
+    url: 'https://budy.gp',
+    image: '/images/portfolio.9.png',
+    description: `Une application Click & Collect et bons plans.<br><small>React Native, Nest.js, MongoDB</small>`,
+  },
+  {
     title: 'Ce portfolio',
     url: 'https://stephenbolivar.com',
     image: '/images/portfolio.1.png',
-    description: `Une PWA réalisée avec ReactJS et déployée sur netlify.`,
-  },
-  {
-    title: 'Site internet statique',
-    url: 'https://budy.gp',
-    image: '/images/portfolio.8.png',
-    description: `Un site statique réalisé à partir d'un template SVG. Compilation statique Nuxt.js`,
+    description: `Site web personnel à visée SEO.<br><small>React,Netlify</small>`,
   },
   {
     title: 'Frontend Vue.js',
     url: 'https://admin.zeewifi.com',
     image: '/images/portfolio.5.png',
-    description: `Un tableau de bord réalisé avec VueJs. Backend en laravel.`,
+    description: `Tableau de bord d'une application web marketing.<br><small>Vue, Laravel</small>`,
   },
   {
     title: 'Frontend Nuxt.js',
     url: undefined,
     image: '/images/portfolio.6.png',
-    description: `Un portail captif réalisé avec NuxtJS + Vuex. Backend en laravel.`,
+    description: `Un portail captif. Authentification à un réseau WIFI ouvert.<br><small>Vue (Nuxt), Laravel, Radius</small>`,
   },
   {
     title: 'Progressive Web App ZeeWIFI',
     url: 'https://app.zeewifi.com',
     image: '/images/portfolio.7.png',
-    description: `Une application simple permetant d'afficher une carte interactive simple. Réalisée avec le framework Quasar`,
+    description: `Une application simple permetant d'afficher une carte interactive simple. <br><small>Vue (Quasar)</small>`,
   },
   {
     title: 'Tiling Window Manager Notification',
     url: 'https://github.com/sboli/twmn',
     image: '/images/portfolio.2.png',
-    description: `Une application C++/Qt, client-serveur permettant d'afficher des notifications
-        correctement intégrées sur les gestionnaires des fenêtres dont tout l'espace est occupé par le contenu.`,
+    description: `Une application de bureau, permettant d'afficher des notifications
+        correctement intégrées sur les gestionnaires des fenêtres dont tout l'espace est occupé par le contenu (tiling wm).<br><small>C++, Qt, boost</small>`,
   },
   {
     title: 'Dominoes multiplayer',
     url:
       'https://play.google.com/store/apps/details?id=com.enjoyinformatique.dwf',
     image: '/images/portfolio.3.png',
-    description: `Un jeu de dominos sur Android réalisé en Java avec <a href="https://libgdx.badlogicgames.com/">libGDX</a>, et propulsé par un serveur NodeJS avec une communication WebSocket.`,
+    description: `Un jeu de dominos multijoueurs en ligne sur Android;<br><small>Java, LibGDX, Websocket, Node.js<small>`,
   },
   {
     title: 'Frontend OCS Ericsson',
     url: undefined,
     image: '/images/portfolio.4.png',
-    description: `Une application PHP/Javascript qui permet de manager l'OCS Ericsson via son api XMLRPC`,
+    description: `Une application d'interface avec l'OCS Ericsson via son api XMLRPC. <br><small>PHP, JQuery</small>`,
   },
 ];
 
